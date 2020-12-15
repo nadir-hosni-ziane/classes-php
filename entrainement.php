ce92f66b782a71e14917f04a663737d977e9b5da
nadir-ziane <nadir.ziane@laplateforme.io>
2020-12-15 16:08:17 +0100

    classes

---
+++ entrainement.php
@@ -0,0 +1,55 @@
+<?php
+
+require "lpdo.php";
+
+//$user = new Lpdo('localhost', 'root', '', 'classes');
+
+//$result = $user->execute("SELECT * from utilisateurs");
+
+//$result = $user->getFields('utilisateurs');
+
+//var_dump($result);
+
+//$user->connect('localhost', 'root', '', 'classes');
+
+//$link = mysqli_connect($_host, $_username, $_password, $_db);
+//$this->_link = $link;
+//var_dump($link);
+
+//$values = $user->register('Fa','ziane','ziane.fa13@gmail.com','Fa', 'Ziane');
+//var_dump($values);
+
+//$values = $user->register('nadir','ziane','ziane.nadir13@gmail.com','Nadir', 'Ziane');
+
+//var_dump($values);
+
+//$values2= $user->connect('elias','ziane');
+
+//var_dump($values2);
+
+//$user->disconnect();
+
+//var_dump($user);
+
+//$user->delete();
+
+//$values= $user->connect('robin','arbona');
+
+//$user->update('elias', 'ziane', 'ziane.elias@gmail.com', 'elias', 'ziane');
+
+//var_dump($values);
+
+//$values3 = $user->isConnected();
+
+//var_dump($values3);
+
+//$values4 = $user->getAllInfos();
+
+//$values4 = $user->getAllInfos();
+
+//var_dump($values4);
+
+//$user->refresh();
+//var_dump($user);
+
+?>
